143d488d5af12587f006a50f9528e1d09bd628f8
AshkanGoharfar <ashkan.goharfar@gmail.com>
2020-06-13 20:55:46 +0430

    Proof Heap's law in a plot

--- Zipf and Heap.py
+++ Zipf and Heap.py
@@ -6,36 +6,39 @@ import math
 inverted_index = extract_inverted_index()
 
 
-def Heaps_low(k):
-    # # vocabulary size
-    # m = len(extract_inverted_index())
-    #
-    # # number of tokens
-    # t = len(contents)
-    #
-    # # static coefficient
-    # b = 0.5
-    #
-    # x = math.log10(t)
-    # y = math.log10(m)
-    # area = np.pi * 3
-    #
-    # # Plot
-    # plt.scatter(x, y, s=area, alpha=0.5)
-    #
-    # plt.title('Comaparison dataset columns')
-    # plt.xlabel('vocab size')
-    # plt.ylabel('number of tokens')
-    #
-    # plt.plot()
-    # plt.show()
+freq = []
 
-    i = 0
-    words = set()
-    for word in inverted_index:
-        words.add(word)
-        i += 1
-        print(i, len(words))
+for item in inverted_index:
+    freq.append(inverted_index[item]['freq'])
 
 
-Heaps_low(40)
+def Heaps_law(k):
+    x = []
+    y = []
+    token = 0
+    for i in range(1, len(freq)):
+        x.append(math.log10(i))
+        token += freq[i - 1]
+        y.append(math.log10(token))
+    area = np.pi * 3
+
+    # Plot
+    plt.scatter(x, y, s=area, alpha=0.5)
+
+    plt.title('Comaparison dataset columns')
+    plt.xlabel('vocab size')
+    plt.ylabel('number of tokens')
+
+    plt.plot()
+    plt.show()
+
+
+# def Zipf_law():
+    # x = []
+    # y = []
+    # token = 0
+    # for i in range(len(freq)):
+    #     x.append(math.log10())
+
+
+Heaps_law(40)
