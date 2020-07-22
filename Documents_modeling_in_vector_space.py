6a99fa9cd62f5e37154f36591500bc628e8c13ef
AshkanGoharfar <ashkan.goharfar@gmail.com>
2020-07-22 22:48:49 +0430

    Implement index eliminate

--- Documents_modeling_in_vector_space.py
+++ Documents_modeling_in_vector_space.py
@@ -8,31 +8,8 @@ import math
 lemmatizer = Lemmatizer()
 all_of_contents, inverted_index = extract_inverted_index()
 
-######################### nan array for query words ###################################
-# nan_array = []
-# for i in range(len(query_words)):
-#     nan_array.append(0)
-#################### End #########################
-
-
 docs_dict = {}
 
-############ document dictionary for query's words ###################
-# k = 0
-# for i in range(len(query_words)):
-#     if query_words[i] in inverted_index:
-#         # print(inverted_index[query_words[i]]['doc'])
-#         for j in range(len(inverted_index[query_words[i]]['doc'])):
-#             if inverted_index[query_words[i]]['doc'][j] not in docs_dict:
-#                 docs_dict[inverted_index[query_words[i]]['doc'][j]] = {'words': nan_array}
-#             else:
-#                 docs_dict[inverted_index[query_words[i]]['doc'][j]]['words'][k] += 1
-#         k += 1
-#         # docs_dict[inverted_index[query_words[i]]['doc']] = nan_array
-############ End ###################
-
-
-########################### bayad dictionary ijad konam ke key hash baraye har doc bashe va toosh bege har kalame chand bar tekrar shode
 for i in range(len(merge_docs()['all_docs'])):
     nan_array = []
     for term in inverted_index:
@@ -42,14 +19,6 @@ for i in range(len(merge_docs()['all_docs'])):
 all_terms = []
 k = 0
 for term in inverted_index:
-    # for j in range(len(inverted_index[term]['doc'])):
-    #     # if inverted_index[term]['doc'][j] not in docs_dict:
-    #     docs_dict[inverted_index[term]['doc'][j]]['words'][k] = 1
-    #     # nan_array = []
-    #     # for i in range(len(inverted_index)):
-    #     #     nan_array.append(0)
-    #     # nan_array[k] = 1
-    #     # docs_dict[inverted_index[term]['doc'][j]] = {'words': nan_array}
     all_terms.append(term)
     k += 1
 
@@ -62,32 +31,6 @@ print('docs_dict : ')
 for item in docs_dict:
     print(item)
     print(docs_dict[item]['words'])
-
-
-# print(docs_dict)
-# for doc in docs_dict:
-#     for j in range(len(docs_dict[doc]['words'])):
-#         if docs_dict[doc]['words'][j] > 0:
-#
-
-############################ last code of terms frequency in each doc
-# k = 0
-# for item in inverted_index:
-#     # if query_words[i] in inverted_index:
-#         # print(inverted_index[query_words[i]]['doc'])
-#     for j in range(len(inverted_index[item]['doc'])):
-#         if inverted_index[item]['doc'][j] not in docs_dict:
-#             nan_array = []
-#             for i in range(len(inverted_index)):
-#                 nan_array.append(0)
-#             nan_array[k] = 1
-#             docs_dict[inverted_index[item]['doc'][j]] = {'words': nan_array, 'term': item}
-#             # print('new doc in dict : ', docs_dict[inverted_index[item]['doc'][j]])
-#         else:
-#             docs_dict[inverted_index[item]['doc'][j]]['words'][k] += 1
-#             # print('repeated doc in dict : ', docs_dict[inverted_index[item]['doc'][j]]['words'][k])
-#     k += 1
-#     # docs_dict[inverted_index[query_words[i]]['doc']] = nan_array
 
 
 def num_of_docs():
@@ -103,9 +46,10 @@ def num_of_docs():
 
 print('num of docs : ', num_of_docs())
 
+term_in_doc = []
+
 
 def calculate_tf_idf(term, doc, doc_coll):
-    # tf_idf = 0
     print('Fucking length : ', len(docs_dict[doc]['words']))
     if term in inverted_index:
         print('selected document : ', doc)
@@ -123,6 +67,13 @@ print(calculate_tf_idf(list(inverted_index.keys())[0], 1, num_of_docs()))
 query = "صدا و سیما"
 query_words = query.split(' ')
 
+# Index elimination for multiple query
+stop_words = collect_stop_words()
+for word in query_words:
+    if word in stop_words:
+        query_words.remove(word)
+
+
 for item in query_words:
     if '#' in lemmatizer.lemmatize(item):
         # print('item before stemming : ', item)
@@ -132,104 +83,20 @@ for item in query_words:
 
 def score(document, query_words):
     score = 0
-    # docs = 0
-    # for documents in docs_dict:
-    #     for query in query_words:
-    #         if docs_dict[documents]['words'][all_terms.index(query)] != 0:
-    #             print('This document contain this term : ', documents)
-    # document = docs
     for term in query_words:
-        # for i in range(len(list(docs_dict.keys()))):
         if docs_dict[int(document)]['words'][all_terms.index(term)] != 0:
             score += calculate_tf_idf(term, document, num_of_docs())
     return score
 
 
-##################### result should use instad of 1 input for score function
+for term in query_words:
+    for docs in docs_dict:
+        if docs_dict[docs]['words'][all_terms.index(term)] != 0:
+            term_in_doc.append(docs)
+            print('which doc : ', docs)
 
 print(inverted_index[query_words[0]]['doc'])
 
-print(score(14, query_words))
-
-# def calculate_tf_idf():
-#     tf_idf = []
-#     ############### calculate tf-idf for query's words ###################
-#     # for i in range(len(query_words)):
-#     #     word_tf = 0
-#     #     for item in docs_dict:
-#     #         # print(docs_dict[item]['words'][i])
-#     #         if docs_dict[item]['words'][i] > 0:
-#     #             word_tf += (1 + math.log10(docs_dict[item]['words'][i]))
-#     #     print(num_of_docs())
-#     #     print(inverted_index[query_words[i]]['freq'])
-#     #     word_idf = num_of_docs() / inverted_index[query_words[i]]['freq']
-#     #     tf_idf.append(word_tf - word_idf)
-#     ############ calculate tf-idf for query's words ###################
-#
-#     # l = 0
-#     # for elem in inverted_index:
-#     #     word_tf = 0
-#     #     for item in docs_dict:
-#     #         # print(docs_dict[item]['words'][i])
-#     #         if docs_dict[item]['words'][l] > 0:
-#     #             # print('doc value', docs_dict[item]['words'][l])
-#     #             word_tf += (1 + math.log10(docs_dict[item]['words'][l]))
-#     #     # print(num_of_docs())
-#     #     # print(inverted_index[elem]['freq'])
-#     #     word_idf = num_of_docs() / inverted_index[elem]['freq']
-#     #     tf_idf.append(word_tf - word_idf)
-#     #     l += 1
-#
-#     l = 0
-#     doc = list(docs_dict.keys())[0]
-#     print('first doc : ', doc)
-#     # for elem in docs_dict:
-#     # doc = docs_dict.keys(0)
-#
-#     ######################## barresi e item haye dakhele iverted index va be kar girish shooon bara ye kolan invrted_index.keys(l) ?? !! ########
-#     # all_term = list(inverted_index.keys())
-#     doc_term = []
-#     which_doc = 0
-#     # print(docs_dict[list(docs_dict.keys())[which_doc]]['words'])
-#     # print(docs_dict[list(docs_dict.keys())[1]]['words'])
-#     for i in range(len(docs_dict[list(docs_dict.keys())[which_doc]]['words'])):
-#         if docs_dict[list(docs_dict.keys())[which_doc]]['words'][i] > 0:
-#             doc_term.append([list(inverted_index.keys())[i], i])
-#     word_tf = 0
-#     for term in doc_term:
-#         for item in docs_dict:
-#             print('word freq in each doc : ', docs_dict[item]['words'][term[1]])
-#             ################## error !!!!!!!!!!!!!!
-#             word_tf += (1 + math.log10(docs_dict[item]['words'][term[1]]))
-#         print('sum of word_tf', word_tf)
-#         word_idf = num_of_docs() / inverted_index[term[0]]['freq']
-#         print('word_idf', word_idf)
-#         print('each element word_tf - word_idf : ', word_tf - word_idf)
-#         tf_idf.append(word_tf - word_idf)
-#
-#     # for item in doc_term:
-#     #     if docs_dict[docs_dict.keys(0)]['word']
-#     # word_tf = 0
-#     # for item in docs_dict:
-#     #     # print(docs_dict[item]['words'][i])
-#     #     if docs_dict[item]['words'][l] > 0:
-#     #         # print('doc value', docs_dict[item]['words'][l])
-#     #         word_tf += (1 + math.log10(docs_dict[item]['words'][l]))
-#     #     l += 1
-#     # print(num_of_docs())
-#     # print(inverted_index[elem]['freq'])
-#
-#     return tf_idf
-
-
-# j = 0
-# for i in range(len(query_words)):
-#     if query_words[i] in inverted_index:
-#         for k in range(len(inverted_index[query_words[i]]['doc'])):
-#             if inverted_index[query_words[i]]['doc'][k] not in docs_dict:
-#                 docs_dict[inverted_index[query_words[i]]['doc'][k]][j] = 1
-#             else:
-#                 docs_dict[inverted_index[query_words[i]]['doc'][k]][j] += 1
-#         j += 1
-
-# print(docs_dict)
+for best_doc in term_in_doc:
+    print('best_doc : ', best_doc)
+    print('score for best doc : ', score(best_doc, query_words))
